92e1af50c362b23169b5d941f715ff33a2d8c8b2
Marco Perone <pasafama@gmail.com>
2020-03-06 17:27:15 +0100

    sorting is invariant of a permutation of the input

--- test/SortSpec.hs
+++ test/SortSpec.hs
@@ -1,14 +1,25 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE FlexibleInstances   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module SortSpec where
 
+import Data.Foldable (toList)
 import Data.List (delete, sort)
 
 -- extra
 import Data.List.Extra (snoc)
 
+-- Fin
+import Data.Fin as Fin
+import Data.Fin.List hiding (swap)
+import Data.Fin.Permutation
+
 -- hspec
 import Test.Hspec
+
+-- peano
+import Data.Peano as Peano
 
 -- QuickCheck
 import Test.QuickCheck
@@ -20,11 +31,24 @@ import Safe (minimumMay, maximumMay)
 preserves :: Eq a => [a] -> [a] -> Bool
 preserves [] [] = True
 preserves (x : xs) ys = x `elem` ys && preserves xs (delete x ys)
+preserves _ _ = False
 
 ordered :: Ord a => [a] -> Bool
 ordered [] = True
-ordered [x] = True
+ordered [_] = True
 ordered (x : y : xs) = x <= y && ordered (y : xs)
+
+instance Arbitrary a => Arbitrary (List ('Peano.Succ ('Peano.Succ ('Peano.Succ ('Peano.Succ 'Peano.Zero)))) a) where
+  arbitrary = (:.) <$> arbitrary <*> ((:.) <$> arbitrary <*> ((:.) <$> arbitrary <*> ((:.) <$> arbitrary <*> pure Nil)))
+  shrink = (: [])
+
+instance Arbitrary (Fin.Fin ('Peano.Succ ('Peano.Succ ('Peano.Succ ('Peano.Succ 'Peano.Zero))))) where
+  arbitrary = oneof [pure Fin.Zero, pure $ Fin.Succ Fin.Zero, pure $ Fin.Succ (Fin.Succ Fin.Zero)]
+  shrink = (: [])
+
+instance Arbitrary (Permutation ('Peano.Succ ('Peano.Succ ('Peano.Succ ('Peano.Succ 'Peano.Zero))))) where
+  arbitrary = mconcat <$> listOf (swap <$> arbitrary <*> arbitrary)
+  shrink = (: [])
 
 spec :: Spec
 spec = do
@@ -56,3 +80,9 @@ spec = do
     it "puts the minimum at the beginning" $ property $
       \(x :: String) -> let xmin = minimumMay x
                         in  (flip (:) (sort x) <$> xmin) == (sort . snoc x <$> xmin)
+
+    -- this is beaten by (const [])
+    it "produces the same result for any permutation of the input" $ property $
+      \( x :: List ('Peano.Succ ('Peano.Succ ('Peano.Succ ('Peano.Succ 'Peano.Zero)))) Char
+       , p :: Permutation ('Peano.Succ ('Peano.Succ ('Peano.Succ ('Peano.Succ 'Peano.Zero))))
+       ) -> sort (toList x) == (sort $ toList (apply p x))
